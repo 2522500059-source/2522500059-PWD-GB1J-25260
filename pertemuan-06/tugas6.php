6e527abb1d397ff5774e71c4241ad34c3997e05b
Annisa <2522500059@mahasiswa.atmaluhur.ac.id>
2025-11-07 19:50:04 +0700

    download dan extrac all pertemuan 07

--- pertemuan-06/tugas6.php
+++ pertemuan-06/tugas6.php
@@ -414,6 +414,6 @@
         <p><strong>Total Bobot:</strong> <?php echo $totalbobot; ?></p>
         <p><strong>Total SKS:</strong> <?php echo $totalsks; ?></p>
         <p><strong>IPK:</strong> <?php echo number_format($ipk, 2); ?></p>
-   </section>
+    </section>
     </body>
 </html>
--- pertemuan-07/README.md
+++ pertemuan-07/README.md
@@ -1,1 +1,8 @@
 # pertemuan-07
+
+Nim : 2522500059
+Nama : Annisa
+
+Hari ini tanggal 06 november 2025,saya mempelajari :
+<ol>
+<li>download dan extrac all pertemuan 07<li>
